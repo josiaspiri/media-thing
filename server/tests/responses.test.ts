53c5e09fdb35dfe6a750e0d6d0419a4f51538349
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2026-04-25 19:59:36 -0400

    test: unsatisfiable testing

---
+++ server/tests/responses.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "bun:test";
+import { RESPONSES } from "../responses";
+
+describe("responses.unsatisfiable", () => {
+  test("responds with 416", () => {
+    expect(RESPONSES.UNSATISFIABLE(123).status).toBe(416);
+  });
+
+  test("responds with appropriate range header", () => {
+    expect(RESPONSES.UNSATISFIABLE(123).headers.get("Content-Range")).toBe("bytes */123");
+  });
+});
