9851dce310bbcb644e7b71d657c6d862b2b3f2a6
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2026-05-14 20:17:55 -0400

    chore: format

--- server/lib/ffprobe.ts
+++ server/lib/ffprobe.ts
@@ -3,7 +3,7 @@ import type { Stream } from "../types/ffprobe";
 const ffprobe = async <T = any>(args: string[]): Promise<T> => {
   const proc = Bun.spawn(
     ["ffprobe", "-v", "quiet", "-print_format", "json", ...args],
-    { stderr: "ignore", stdout: "pipe",  },
+    { stderr: "ignore", stdout: "pipe" },
   );
   const [data, code] = await Promise.all([proc.stdout.json(), proc.exited]);
   if (code !== 0) throw new Error(`ffprobe error ${code}`);
